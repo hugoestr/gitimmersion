2ba03cd5e8055944b459ad033766809b013e46df
Hugo Estrada <Hugo.Estrada@afge.org>
2016-04-14 20:30:00 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Hugo Estrada (email@email.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!" 
+greeter = Greeter.new name
 
+puts greeter.greet
